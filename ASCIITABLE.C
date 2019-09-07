78b1d4a89e7a386b2bae869f87e3392b2c655ae7
emmegha <41463313+emmegha@users.noreply.github.com>
2019-09-07 23:14:48 +0530

    Add files via upload

---
+++ ASCIITABLE.C
@@ -0,0 +1,15 @@
+//program to print ascii value
+
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+ int i;
+ for(i=0;i<=255;i++)
+ {
+ printf(" the ASCII value of %c Is %d\n",i,i);
+ }
+
+ getch();
+ }
